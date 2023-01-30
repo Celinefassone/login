64ed65a48a42fedd4bdb981c151c435ee0713aae
Celine Fassone <celinefassone@MacBook-Air-di-Celine.local>
2023-01-30 13:52:18 +0100

    new commit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   const myButton = document.querySelector("#btn");
+  let form = document.querySelector("#form");
   myButton.addEventListener("click", function () {
-    console.log(this);
+    console.log(form.password.value);
+    console.log(form.username.value);
+    console.log(form.checkbox.value);
   });
 });
