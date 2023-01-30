eb5c164bbd4034020d40447a624c34da0412e1f4
Digital Knight <digitalknight@Digitals-MacBook-Pro.local>
2023-01-30 14:01:19 +0100

    Added flex example

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   const myButton = document.querySelector("#btn");
   let form = document.querySelector("#form");
-  myButton.addEventListener("click", function () {
-    console.log(form.password.value);
-    console.log(form.username.value);
-    console.log(form.checkbox.value);
-  });
+
+  if (myButton) {
+    myButton.addEventListener("click", function () {
+      console.log(form.password.value);
+      console.log(form.username.value);
+      console.log(form.checkbox.value);
+    });
+  }
 });
